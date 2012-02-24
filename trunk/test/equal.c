182b6c95c81e6d4809d2080da84bb58815e3ab15
njucslzh0714@gmail.com <njucslzh0714@gmail.com@6e9ba42f-1bf9-061a-214a-5c0899d55ead>
2012-02-24 15:13:43 +0000

    考虑数组；

--- trunk/test/equal.c
+++ trunk/test/equal.c
@@ -1,35 +1,4 @@
 /*@
-	@predicate is_dense_increase(int x) = 
-	@	x == x+1;
-	@
-	@predicate is_positive(int x) = 
-	@	0<x;
-	@	
-	@predicate is_negative(int x) =
-	@	x<0;
-	@
-	@predicate is_valid_index(int *arr,int i,int n) =
-	@	(0 <= i) && (i < n);
-	@
-	@predicate is_valid_index (int arr[],int i,int n) =
-	@	(0<=i) && (i < n);
-	@
-	@predicate elements_dec(int *a,int len) = 
-	@	\forall integer i,j; 0 <= i <= j < len ==> a[i] > a[j];
-	@
-	@predicate elements_eq(int *a,int len) = 
-	@	\forall integer i; 0 <= i < len ==> a[i] == a[0];
-	@
-	@predicate elements_inc(int *a,int len) = 
-	@	\forall integer i,j; 0 <= i <= j < len ==> a[i] < a[j];
-	@
-	@predicate elements_part_eq(int *a,int *b,int i) =
-	@	\forall int k; 0 <= k < i ==> a[k] == b[k];
-	@
-*/
-
-
-/*@
    @requires \valid_range(a,0,n-1);
    @requires \valid_range(b,0,n-1);
 @
@@ -52,4 +21,10 @@ int equal(int* a, int n, int* b)
      if (a[i] != b[i])
        return 0;
 	return 1;
+}
+
+void main()
+{
+	int a[]={1,2,3},b[]={1,2,4};
+	equal(a,3,b);
 }
