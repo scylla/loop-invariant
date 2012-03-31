588457e39c98a21cf205e35d3b6ea7e0ae354bb8
njucslzh0714 <njucslzh0714@gmail.com>
2012-03-31 07:57:34 +0000

    大致先这样，赶紧写完论文；

--- frontend.ml
+++ frontend.ml
@@ -4,13 +4,13 @@ open Template
 open Cil_types
 open Loop_parameters
   
-let build_graphs (fmt:Format.formatter) (prog:Equation.info) arrayvars ipl wp_compute unknownout :graph * graph =
+let build_graphs (fmt:Format.formatter) (prog:Equation.info) arrayvars ipl wp_compute annots unknownout :graph * graph =
   (* Converting prog into a forward equation system *)
-  let (fgraph:graph) = C2equation.Forward.make prog arrayvars fmt ipl wp_compute unknownout in
-	(*fprintf fmt "print fgraph after ok 1\n";
+  let (fgraph:graph) = C2equation.Forward.make prog arrayvars fmt ipl wp_compute annots unknownout in
+	fprintf fmt "print fgraph after ok 1\n";
   Equation.print_graph fmt fgraph;
 	fprintf fmt "print fgraph after ok 2\n";
-  Converting prog into a backward equation system *)
+ (* Converting prog into a backward equation system *)
   (*let (bgraph:graph) = C2equation.Backward.make prog in
   fprintf fmt "print bgraph after ok 1\n";
   Equation.print_graph fmt bgraph;
@@ -18,36 +18,38 @@ let build_graphs (fmt:Format.formatter) (prog:Equation.info) arrayvars ipl wp_co
 	let bgraph = fgraph in
   (fgraph,bgraph)
 		
-let compute_and_display (fmt:Format.formatter) (prog:Equation.info) (fgraph:Equation.graph) (bgraph:Equation.graph) (manager:'a Apron.Manager.t) : unit =
+let compute_and_display (fmt:Format.formatter) (prog:Equation.info) (fgraph:Equation.graph) (bgraph:Equation.graph) (manager:'a Apron.Manager.t) annots : unit =
   let (previous:(Equation.point, int, 'a Apron.Abstract1.t, Equation.transfer) Fixpoint.output option ref) =
     ref None
   in
-  List.iter(fun t ->
+  
+  Hashtbl.iter(fun pname _->
+  	List.iter(fun t ->
       (* Computation *)
-  	let fp =
-			begin match t with
-			| Loop_parameters.Forward ->
-				Printf.printf "Forward\n";
-				let fp =
-					Template.Forward.compute ~fmt fgraph ~output:(!previous) manager ~debug:1
-				in
-				fp
-			| Loop_parameters.Backward ->
-				Printf.printf "Backward\n";
-				let fp =
-					Template.Backward.compute ~fmt bgraph ~output:(!previous) manager ~debug:1
-				in
-				fp
-			end
-    in
-      (* Apply and Display *)
-    previous := Some fp;
-    Apply.apply_result prog fmt fp;
-    (*Template.print_output prog fmt fp;
-    match !previous with
-    | Some(out)->
-      Printf.printf "previous is some\n";
-    | None->
-      Printf.printf "previous is none\n";*)
-  )!analysis;
-  ()
+			let fp =
+				begin match t with
+				| Loop_parameters.Forward ->
+					Printf.printf "Forward:%s\n" pname;
+					let fp =
+						Template.Forward.compute ~fmt fgraph pname ~output:(!previous) manager annots ~debug:1
+					in
+					fp
+				| Loop_parameters.Backward ->
+					Printf.printf "Backward\n";
+					let fp =
+						Template.Backward.compute ~fmt bgraph ~output:(!previous) manager ~debug:1
+					in
+					fp
+				end
+		  in
+		    (* Apply and Display *)
+		  previous := Some fp;
+		  Apply.apply_result prog fmt fp annots;
+		  (*Template.print_output prog fmt fp;
+		  match !previous with
+		  | Some(out)->
+		    Printf.printf "previous is some\n";
+		  | None->
+		    Printf.printf "previous is none\n";*)
+		)!analysis;
+  )prog.Equation.procinfo
